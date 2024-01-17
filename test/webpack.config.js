1e0f6180ceb5994b3889719328fb5cf56d5282c0
0xpapercut <0xpapercut@gmail.com>
2024-01-18 00:09:56 +0100

    Update test webpack config

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -18,4 +18,18 @@ module.exports = {
     experiments: {
         asyncWebAssembly: true,
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules\/(?!katex)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-flow'],
+                    },
+                },
+            },
+        ]
+    }
 };
