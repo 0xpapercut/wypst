a48262f082d0009646fc07ff3685b35f1d6b8269
0xpapercut <0xpapercut@gmail.com>
2024-03-02 01:26:41 +0100

    Add renderError function to KaTeX bundle

--- src/katex/katex.js
+++ src/katex/katex.js
@@ -2,10 +2,23 @@ import ParseError from 'katex/src/ParseError';
 import Settings from 'katex/src/Settings';
 import parseTree from 'katex/src/parseTree';
 import buildTree from 'katex/src/buildTree';
+import buildCommon from 'katex/src/buildCommon';
+import { SymbolNode } from 'katex/src/domTree';
+
+function renderError(error, expression, settings) {
+    if (settings.throwOnError) {
+        throw error;
+    }
+    const node = buildCommon.makeSpan(["katex-error"], [new SymbolNode(expression)]);
+    node.setAttribute("title", error.toString());
+    node.setAttribute("style", `color:${settings.errorColor}`);
+    return node;
+}
 
 export default {
     ParseError,
     Settings,
     parseTree,
     buildTree,
+    renderError,
 };
