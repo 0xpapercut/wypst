2a3b0c482173d022769714b56929015e3b05779c
0xpapercut <0xpapercut@gmail.com>
2024-05-03 21:18:18 +0200

    Update build script

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,15 +1,41 @@
 import esbuild from 'esbuild';
+import fs from 'fs';
 
+const entryPoint = './wypst.js';
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+// Inline wasm build
 esbuild.build({
-    entryPoints: ['./wypst.js'],
+    entryPoints: [entryPoint],
     bundle: true,
-    minify: true,
-    sourcemap: true,
+    minify: !isDevelopment,
+    sourcemap: isDevelopment,
     target: ['es6'],
-    outfile: './dist/wypst_inlined.js',
+    outfile: './dist/wypst.min.js',
     format: 'iife',
     globalName: 'wypst',
     loader: {
         '.wasm': 'binary'
     },
 });
+
+// Normal build
+esbuild.build({
+    entryPoints: [entryPoint],
+    bundle: true,
+    minify: !isDevelopment,
+    sourcemap: isDevelopment,
+    target: ['es6'],
+    outfile: './dist/wypst.js',
+    format: 'iife',
+    globalName: 'wypst',
+    loader: {
+        '.wasm': 'file'
+    },
+    metafile: true,
+    assetNames: 'wypst',
+});
+
+// Copy CSS files
+fs.copyFileSync('node_modules/katex/dist/katex.css', 'dist/wypst.css');
+fs.copyFileSync('node_modules/katex/dist/katex.min.css', 'dist/wypst.min.css');
